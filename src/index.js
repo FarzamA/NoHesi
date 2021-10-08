3e79cb0abbfd217d73cc98234d892504a557e4c8
Farzam Ahmad <farzam135@gmail.com>
2021-10-08 14:33:45 -0400

    working importing orbital controls for testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const Game = require("../src/scripts/game");
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
+
+    const g = new Game();
 });
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,9 +1,9 @@
 const THREE = require('three');
-// Added for developer purposes not intending to keep
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// // Added for developer purposes not intending to keep
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// in order to attempt to load a figure
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// // in order to attempt to load a figure
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
 // Game class to hold all game logic
 class Game {
@@ -18,8 +18,11 @@ class Game {
 
         // Create scene and camera to be rendered later
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PrespectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000 );
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000 );
         this.camera.position.set(1, 1, 5); 
+
+        // Remember to select the canvas to render to and hand it to the renderer
+        const canvas = document.querySelector("#game");
 
         // Create renderer to render and handle our scene
         this.renderer = new THREE.WebGL1Renderer();
@@ -42,13 +45,14 @@ class Game {
 
         // takes in a destination url for assets, what to do on the load, what to do during the load
         // and what to do if there is an error
-        loader.load("", function(object) {
-            that.playerCar = object;
-            that.scene.add(object); 
+        loader.load("", function(objects) {
+            const cars = objects.array;
+            that.playerCar = object[0];
+            that.scene.add( playerCar ); 
             // might need to update or set controls for the actual car here 
 
             // add shadows to everything that is related to the car
-            object.traverse( function(child) {
+            playerCar.traverse( function(child) {
                 // make sure to only add shadows to meshes
                 if (child.isMesh){
                     child.castShadow = true; 
@@ -56,8 +60,8 @@ class Game {
                 };
             });
 
-            // animate the whole game once the actual file we are loading has loaded
-            that.animate();
+                // animate the whole game once the actual file we are loading has loaded
+                that.animate();
 
         }, null, function(error) {
             console.log(error);
@@ -78,4 +82,6 @@ class Game {
 
         this.renderer.render ( this.scene, this.camera );
     };
-}
+};
+
+module.exports = Game;
